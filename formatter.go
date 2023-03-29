6b6c18c0bdeb6483f1095dd800e190212e295fe7
Flc゛ <four_leaf_clover@foxmail.com>
2023-03-29 19:31:03 +0800

    improve: optimize formatterable (#2)

--- formatter.go
+++ formatter.go
@@ -5,3 +5,25 @@ type Formatter interface {
 }
 
 type FormatterOpt func(Formatter)
+
+type Formatterable struct {
+	formatter Formatter
+}
+
+func NewFormatterable(formatter Formatter) *Formatterable {
+	return &Formatterable{
+		formatter: formatter,
+	}
+}
+
+func (f *Formatterable) Format(record *Record) string {
+	return f.formatter.Format(record)
+}
+
+func (f *Formatterable) SetFormatter(formatter Formatter) {
+	f.formatter = formatter
+}
+
+func (f *Formatterable) GetFormatter() Formatter {
+	return f.formatter
+}
