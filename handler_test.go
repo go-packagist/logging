22a8ddb0663b4aa24d4dd3376b90c3e7cc583192
Flc゛ <four_leaf_clover@foxmail.com>
2023-04-02 19:04:18 +0800

    test: improve single test coverage (#12)

--- handler_test.go
+++ handler_test.go
@@ -1,9 +1,29 @@
 package monolog
 
 import (
+	"github.com/go-packagist/logger"
+	"github.com/stretchr/testify/assert"
 	"testing"
 )
 
-func TestHandlerable_New(t *testing.T) {
-	// todo
+func TestHandlerable(t *testing.T) {
+	h := NewHandlerable(
+		WithLevel(logger.Notice),
+	)
+
+	assert.True(t, h.IsHandling(&Record{Level: logger.Emergency}))
+	assert.True(t, h.IsHandling(&Record{Level: logger.Alert}))
+	assert.True(t, h.IsHandling(&Record{Level: logger.Critical}))
+	assert.True(t, h.IsHandling(&Record{Level: logger.Error}))
+	assert.True(t, h.IsHandling(&Record{Level: logger.Warning}))
+	assert.True(t, h.IsHandling(&Record{Level: logger.Notice}))
+	assert.False(t, h.IsHandling(&Record{Level: logger.Info}))
+	assert.False(t, h.IsHandling(&Record{Level: logger.Debug}))
+
+	assert.False(t, h.Handle(&Record{}))
+	assert.False(t, h.HandleBatch([]*Record{
+		{
+			Level: logger.Debug,
+		},
+	}))
 }
